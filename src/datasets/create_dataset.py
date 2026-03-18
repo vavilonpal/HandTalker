be3f82f0f6e9cfcefde0a354488cf356d3ffb697
Alexeev <sergey50878@mail.ru>
2026-03-18 13:54:58 +0200

    add: landmark models and action detections

--- src/datasets/create_dataset.py
+++ src/datasets/create_dataset.py
@@ -44,7 +44,7 @@ for dir_ in os.listdir(DATA_DIR):
                 data.append(data_aux)
                 labels.append(dir_)
 
-
+# Save data into file
 f = open('data.pickle', 'wb')
 pkl.dump({'data': data, 'labels': labels}, f)
 f.close()
